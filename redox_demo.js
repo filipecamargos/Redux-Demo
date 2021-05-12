c016c64e4f3560e372bd8aee22cb2d756cff20a7
filipecamargos <filipe.atrabalho@gmail.com>
2021-05-11 21:54:23 -0600

    reducer function and store introduced

--- redox_demo.js
+++ redox_demo.js
@@ -1,6 +1,12 @@
-//This file will be executed in NodeJS
-//Run nom init and npm install redux
-
 //Import redux
 const redux = require('redux');
 
+//Creating a reducer function to get the old state and the action to be dispatched
+const counterReducer = (state, action) => {
+    return {
+        counter: state.counter + 1
+    };
+}
+
+//create a central store to manage some data with the reducer function
+const store = redux.createStore(counterReducer);
