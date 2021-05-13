768ce5941a261d03a741f249b825a6bce7efb66e
filipecamargos <filipe.atrabalho@gmail.com>
2021-05-12 20:10:07 -0600

    action using switch

--- redux_demo.js
+++ redux_demo.js
@@ -4,27 +4,22 @@ const redux = require("redux");
 //Creating a reducer function to get the old state and the action to be dispatched
 const counterReducer = (state = { counter: 0 }, action) => {
   //Can check for the action dispatched
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-    };
+  switch (action.type) {
+    case "increment":
+      return {
+        counter: state.counter + 1,
+      };
+    case "double":
+      return {
+        counter: state.counter * 2,
+      };
+    case "decrement":
+      return {
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
   }
-
-  if (action.type === 'double') {
-    return {
-      counter: state.counter * 2,
-    }
-  }
-
-  if (action.type === 'decrement') {
-    return {
-      counter: state.counter - 1,
-    }
-  }
-
-  //Else default state is returned
-  return state;
-  
 };
 
 //create a central store to manage some data with the reducer function
@@ -49,10 +44,10 @@ store.dispatch({
 
 //another action can be dispatched
 store.dispatch({
-  type: 'double',
-})
+  type: "double",
+});
 
 //another action can be dispatched
 store.dispatch({
-  type: 'decrement',
-})
+  type: "decrement",
+});
